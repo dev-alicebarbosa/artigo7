8ca7e8774fb2deaa3c98430211c226f11adb4212
dev-alicebarbosa <109616463+dev-alicebarbosa@users.noreply.github.com>
2023-05-16 09:14:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let res= window.document.getElementById('resultado')
+
+function acao1(){
+    res.innerHTML += `<p>Você clicou no 1º botão</p>` 
+}
+
+function acao2(){
+    res.innerHTML += `<p>Você clicou no 2º botão</p>` 
+}
+
+function acao3(){
+    res.innerHTML += `<p>Você clicou no 3º botão</p>` 
+}
+
+function acao4(){
+    res.innerHTML += `<p>Você clicou no 4º botão</p>` 
+}
+
